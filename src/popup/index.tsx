c11c3d48cdf45fbd59ac5c5083836881949e6472
ryota-murakami <dojce1048@gmail.com>
2023-09-16 13:46:28 +0900

    up

--- src/popup/index.tsx
+++ src/popup/index.tsx
@@ -3,7 +3,5 @@ import { createRoot } from 'react-dom/client'
 
 import App from './components/app'
 
-import './index.scss'
-
 const root = createRoot(document.querySelector('div#popup'))
 root.render(<App />)
